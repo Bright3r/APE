620cd4698b3fc886cb3b1ba02ecad5799ec4fe95
Bright3r <chasedbarman@gmail.com>
2025-06-03 02:00:18 -0700

    Loaded GL context

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,15 @@
 #include <SDL3/SDL_events.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3/SDL_oldnames.h>
 #include <SDL3/SDL_video.h>
+#include <glad/glad.h>
 #include <iostream>
 
+int width = 1200;
+int height = 800;
+
 SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
 
 int main() {
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -14,9 +18,21 @@ int main() {
 		return SDL_APP_FAILURE;
 	}
 
-	if (!SDL_CreateWindowAndRenderer("APE Physics Engine", 640, 480, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+	window = SDL_CreateWindow("APE Physics Engine", width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+	if (!window) {
 		std::cerr << "SDL_CreateWindowAndRenderer Failed " << SDL_GetError() << std::endl;
 		return SDL_APP_FAILURE;
+	}
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+	if (!context) {
+		std::cerr << "SDL_GL_CreateContext Failed " << SDL_GetError() << std::endl;
+		return SDL_APP_FAILURE;
+	}
+
+	if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress)) {
+	    std::cerr << "Failed to initialize GLAD" << std::endl;
+	    return -1;
 	}
 
 	// Gameloop
@@ -32,16 +48,15 @@ int main() {
 			}
 		}
 
-		SDL_RenderClear(renderer);
+		// Draw
+		glViewport(0, 0, width, height);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
-		SDL_FRect rect = { 100, 100, 200, 200 };
-		SDL_SetRenderDrawColor(renderer, 0, 255,255, 255);
-		SDL_RenderFillRect(renderer, &rect);
-
-		SDL_RenderPresent(renderer);
+		SDL_GL_SwapWindow(window);
 	}
 
-	SDL_DestroyRenderer(renderer);
+	SDL_GL_DestroyContext(context);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0; 
